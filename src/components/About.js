7db88232c39aaec5fb8e0a33a6b4aa58889c8672
Ramya <ramyaiyengar12@gmail.com>
2024-01-12 15:39:21 -0500

    Class Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
+import User from "./User";
+import UserClass from "./UserClass"
+
+
 const About = () =>{
     return(
         <div>
             <h1>About Us</h1>
             <h2>React Application</h2>
+            <User name={"Ram"} Location={"Ayodhya"}/>
+            <UserClass name={"Ram"} location={"Ayodhya"} />
         </div>
     )
 }
