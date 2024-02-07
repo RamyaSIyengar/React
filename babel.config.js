86e5830f25eabb3d1f7fe2038f53a08577225dcc
Ramya <ramyaiyengar12@gmail.com>
2024-02-07 22:27:35 +0530

    contact updated

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,3 @@ module.exports = {
     presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
   };
 
-//   {
-//     "extends": "@parcel/config-default",
-//     "transformers": {
-//       "*.{js,mjs,jsx,cjs,ts,tsx}": [
-//         "@parcel/transformer-js",
-//         "@parcel/transformer-react-refresh-wrap"
-//       ]
-//     }
-//   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 const Contact = () => {
     return(
         <div>
-            <h1>Contact Us</h1>
+            <h1 className="font-bold text-3xl p-5 mx-10 m-5">Contact Us</h1>
+            <form className="m-4 p-4">
+                <label className="font-bold">Name:</label>
+                <input type="text" name="name" placeholder="name" className="border border-gray-700  my-4 mx-4 px-4 "></input><br />
+                <label className="font-bold  ">Message:</label><br/>
+                <textarea type="text" name="message" placeholder="message"  rows="10" cols="30" className="border border-gray-700  my-4 mx-16 px-4"></textarea><br />
+                <button className="bg-gray-400 rounded font-bold p-2 mx-16 my-4">Submit</button>
+            </form>
         </div>
     )
 }
